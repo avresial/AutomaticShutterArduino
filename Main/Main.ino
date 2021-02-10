ede79366daffe312e2484a968170b47941d7461e
MKarkowski <mikikarkowski@gmail.com>
2021-02-10 21:54:41 +0100

    Added core functionality - StepMotor moves

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,13 +1,63 @@
 
 //Initial program
+int CoilPin = 12;
+int ButtonPin = 8;
 
+#define DirPin 2
+#define StepPin 7
+#define stepsPerRevolution 400
+
+bool Dir = true;// if true clockwise
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CoilPin, OUTPUT);
+  pinMode(StepPin, OUTPUT);
+  pinMode(DirPin, OUTPUT);
+  pinMode(ButtonPin, INPUT);
   Serial.begin(9600);
+
 }
 
 void loop() {
-  Serial.println("XD");
-  delay(1000);
+  if (digitalRead(ButtonPin) == 1)
+  {
+    digitalWrite(CoilPin, HIGH);
+    delay(100);
+    int testLaps = 6;
+    for (int j = 0; j < testLaps; j++)
+    {
+      if (Dir)
+      {
+        Serial.println("Clockwise");
+        digitalWrite(DirPin, HIGH);
+      }
+      else {
+        Serial.println("Counter clockwise");
+        digitalWrite(DirPin, LOW);
+      }
+
+      for (int i = 0; i < stepsPerRevolution; i++)
+      {
+        digitalWrite(StepPin, HIGH);
+        delayMicroseconds(4000);
+        digitalWrite(StepPin, LOW);
+        delayMicroseconds(4000);
+      }
+      Dir = !Dir;
+    }
+  }
+  else
+  {
+    digitalWrite(CoilPin, LOW);
+  }
+
+  delay(100);
+
+
+
+
+  //
+
+
+
 }
